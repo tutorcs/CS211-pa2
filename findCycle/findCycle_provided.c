3215ba47a4b5a93841e124067cb8a71ada247c9e
tutorcs <tutorcs@163.com>
2023-02-26 12:18:01 +0800

    WeChat cstutorcs CS211 pa2

---
+++ findCycle/findCycle_provided.c
@@ -0,0 +1,27 @@
+https://tutorcs.com
+WeChat: cstutorcs
+QQ: 749389476
+Email: tutorcs@163.com
+#include "../graphutils.h"
+
+// A program to find a cycle in a directed graph
+
+// You may use DFS or BFS as needed
+/* ... */
+
+int main ( int argc, char* argv[] ) {
+
+    // READ INPUT FILE TO CREATE GRAPH ADJACENCY LIST
+    AdjacencyListNode* adjacencyList;
+    /* ... */
+
+    bool isCyclic = false;
+    for (unsigned source=0; source<graphNodeCount; source++) {
+        /* ... */
+    }
+
+    if (!isCyclic) { printf("DAG\n"); }
+
+    freeAdjList ( graphNodeCount, adjacencyList );
+    return EXIT_SUCCESS;
+}
